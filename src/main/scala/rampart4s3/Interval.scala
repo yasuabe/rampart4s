648a691ac8a066e6e2fda7aabe65b59295c24d7a
yasuabe2613 <yasu@yasuabe.net>
2022-04-24 10:51:23 +0900

    opaque type to type class

--- src/main/scala/rampart4s3/Interval.scala
+++ src/main/scala/rampart4s3/Interval.scala
@@ -3,40 +3,47 @@ package rampart4s3
 import cats.Order
 import cats.Comparison
 
-object Rampart:
-  opaque type Interval[A] = (A, A)
+trait Interval[P, A](using o: Order[A]):
+  def lesser(p: P): A
+  def greater(p: P): A
+  def isEmpty(p: P): Boolean = o.compare(lesser(p), greater(p)) == 0
+  def nonEmpty(p: P): Boolean = !isEmpty(p)
 
-  object Interval:
-    def apply[A](x: A, y: A)(using o: Order[A]): Interval[A] = (o.min(x, y), o.max(x, y))
-  
-  extension [A: Order](x: Interval[A])
-    def lesser: A = x._1
-    def greater: A = x._2
+  def relate(x: P, y: P): Relation =
+    import Comparison.{EqualTo as EQ, LessThan as LT, GreaterThan as GT}
+    import Relation.*
+    def compare(a: A, b: A) = o.comparison(a, b)
 
-    def isEmpty: Boolean = x._1 == x._2
+    ( compare(lesser(x),  lesser(y) ),
+      compare(lesser(x),  greater(y)),
+      compare(greater(x), lesser(y) ),
+      compare(greater(x), greater(y))) match
+      case (_ , _ , LT, _ ) => Before
+      case (_ , GT, _ , _ ) => After
+      case (EQ, _ , _ , EQ) => Equal
+      case (LT, _ , _ , GT) => Contains
+      case (GT, _ , _ , LT) => During
+      case (LT, _ , EQ, LT) => Meets 
+      case (LT,  _, _ , LT) => Overlaps
+      case (LT, _ , GT, EQ) => FinishedBy
+      case (EQ, _ , GT, LT) => Starts
+      case (EQ, LT, _ , GT) => StartedBy
+      case (GT, LT, _ , EQ) => Finishes
+      case (GT, EQ, _ , GT) => MetBy 
+      case (GT, _ , _ , GT) => OverlappedBy
+      case (_ , _ , EQ, _ ) => Overlaps
+      case (_ , EQ, _ , _ ) => OverlappedBy
+
+object Interval:
+  extension [C, A](p: C)(using pi: Interval[C, A])
+    def isEmpty:  Boolean = pi isEmpty p
     def nonEmpty: Boolean = !isEmpty
+    def lesser:   A       = pi lesser p
+    def greater:  A       = pi greater p
 
-    def relate(y: Interval[A]): Relation =
-      import Comparison.{EqualTo as EQ, LessThan as LT, GreaterThan as GT}
-      import Relation.*
-      def compare(a: A, b: A) = summon[Order[A]].comparison(a, b)
+    def relate(q: C): Relation  = pi.relate(p, q)
 
-      ( compare(x.lesser , y.lesser ),
-        compare(x.lesser , y.greater),
-        compare(x.greater, y.lesser ),
-        compare(x.greater, y.greater)) match
-        case (_ , _ , LT, _ ) => Before
-        case (_ , GT, _ , _ ) => After
-        case (EQ, _ , _ , EQ) => Equal
-        case (LT, _ , _ , GT) => Contains
-        case (GT, _ , _ , LT) => During
-        case (LT, _ , EQ, LT) => Meets 
-        case (LT,  _, _ , LT) => Overlaps
-        case (LT, _ , GT, EQ) => FinishedBy
-        case (EQ, _ , GT, LT) => Starts
-        case (EQ, LT, _ , GT) => StartedBy
-        case (GT, LT, _ , EQ) => Finishes
-        case (GT, EQ, _ , GT) => MetBy 
-        case (GT, _ , _ , GT) => OverlappedBy
-        case (_ , _ , EQ, _ ) => Overlaps
-        case (_ , EQ, _ , _ ) => OverlappedBy
+  type Pair[A] = (A, A)
+  def createPairInstance[A](using o: Order[A]): Interval[Pair[A], A] = new Interval[Pair[A], A]:
+    def lesser(p: Pair[A]): A = o.min(p._1, p._2)
+    def greater(p: Pair[A]): A = o.max(p._1, p._2)
--- src/test/scala/rampart4s3/IntervalSuite.scala
+++ src/test/scala/rampart4s3/IntervalSuite.scala
@@ -5,148 +5,147 @@ import org.scalacheck.Prop._
 import org.scalacheck.{ Arbitrary, Gen }
 import org.scalacheck.cats.implicits._
 import cats.Apply
-import Rampart.Interval
+import Interval.*
 
 class IntervalSuite extends ScalaCheckSuite {
 
-  val genSmallInt = Gen.chooseNum(0, 10)
-  val genIntInterval =
-    summon[Apply[Gen]].map2(genSmallInt, genSmallInt)(Interval(_, _))
+  val genSmallInt    = Gen.chooseNum(0, 10)
+  val genIntInterval = summon[Apply[Gen]].map2(genSmallInt, genSmallInt)((_, _))
 
-  given Arbitrary[Interval[Int]] = Arbitrary(genIntInterval)
+  given Arbitrary[(Int, Int)] = Arbitrary(genIntInterval)
+
+  given pi: Interval[(Int, Int), Int] = createPairInstance[Int]
 
   property("lesser is always less than or equals to greater") {
-    forAll { (interval: Interval[Int]) => interval.lesser <= interval.greater }
+    forAll { (i: (Int, Int)) => pi.lesser(i) <= pi.greater(i) }
   }
   property("interval is empty iif lesser equals to greater") {
     given Arbitrary[Int] = Arbitrary(genSmallInt)
-    forAll { (l: Int, g: Int) => Interval(l, g).isEmpty == (l == g) }
+    forAll { (l: Int, g: Int) => (l, g).isEmpty == (l == g) }
   }
   property("interval is nonEmpty iif lesser doesn't equal to greater") {
     given Arbitrary[Int] = Arbitrary(genSmallInt)
-    forAll { (l: Int, g: Int) => Interval(l, g).nonEmpty == (l != g) }
+    forAll { (l: Int, g: Int) => (l, g).nonEmpty == (l != g) }
   }
   property("x.lesser = y.lesser ∧ x.greater = y.greater → Equal") {
-    assert((Interval(1, 2) relate Interval(1, 2)) == Relation.Equal)
-    assert((Interval(2, 2) relate Interval(2, 2)) == Relation.Equal)
+    assert(((1, 2) relate (1, 2)) == Relation.Equal)
+    assert(((2, 2) relate (2, 2)) == Relation.Equal)
 
-    forAll { (x: Interval[Int], y: Interval[Int]) =>
+    forAll { (x: (Int, Int), y: (Int, Int)) =>
       val cond = x.lesser == y.lesser && x.greater == y.greater
       cond == ((x relate y) == Relation.Equal)
     }
   }
   property("x.nonEmpty ∧ y.nonEmpty ∧ x.greater = y.lesser→ Meets") {
-    assert((Interval(1, 2) relate Interval(2, 3)) == Relation.Meets)
+    assert(((1, 2) relate (2, 3)) == Relation.Meets)
 
-    forAll { (x: Interval[Int], y: Interval[Int]) =>
+    forAll { (x: (Int, Int), y: (Int, Int)) =>
       val cond = x.nonEmpty && y.nonEmpty && x.greater == y.lesser
       cond == ((x relate y) == Relation.Meets)
     }
   }
   property("x.nonEmpty ∧ y.nonEmpty ∧ x.greater = y.lesser → MetBy") {
-    assert((Interval(2, 3) relate Interval(1, 2)) == Relation.MetBy)
+    assert(((2, 3) relate (1, 2)) == Relation.MetBy)
 
-    forAll { (x: Interval[Int], y: Interval[Int]) =>
+    forAll { (x: (Int, Int), y: (Int, Int)) =>
       val cond = x.nonEmpty && y.nonEmpty && x.lesser == y.greater
       cond == ((x relate y) == Relation.MetBy)
     }
   }
   property("x.nonEmpty ∧ y.nonEmpty ∧ x.lesser = y.lesser ∧ x.greater < y.greater → Starts") {
-    assert((Interval(3, 4) relate Interval(3, 7)) == Relation.Starts)
+    assert(((3, 4) relate (3, 7)) == Relation.Starts)
 
-    forAll { (x: Interval[Int], y: Interval[Int]) =>
+    forAll { (x: (Int, Int), y: (Int, Int)) =>
       val cond = x.nonEmpty && y.nonEmpty && x.lesser == y.lesser && x.greater < y.greater
       cond == ((x relate y) == Relation.Starts)
     }
   }
   property("x.nonEmpty ∧ y.nonEmpty ∧ x.lesser = y.lesser ∧ x.greater > y.greater → StartedBy") {
-    assert((Interval(3, 8) relate Interval(3, 7)) == Relation.StartedBy)
+    assert(((3, 8) relate (3, 7)) == Relation.StartedBy)
 
-    forAll { (x: Interval[Int], y: Interval[Int]) =>
+    forAll { (x: (Int, Int), y: (Int, Int)) =>
       val cond = x.nonEmpty && y.nonEmpty && x.lesser == y.lesser && x.greater > y.greater
       cond == ((x relate y) == Relation.StartedBy)
     }
   }
   property("x.greater < y.lesser → Before") {
-    assert((Interval(0, 1) relate Interval(2, 2)) == Relation.Before)
-    assert((Interval(1, 1) relate Interval(2, 2)) == Relation.Before)
-    assert((Interval(1, 1) relate Interval(2, 3)) == Relation.Before)
-    assert((Interval(0, 1) relate Interval(2, 3)) == Relation.Before)
+    assert(((0, 1) relate (2, 2)) == Relation.Before)
+    assert(((1, 1) relate (2, 2)) == Relation.Before)
+    assert(((1, 1) relate (2, 3)) == Relation.Before)
+    assert(((0, 1) relate (2, 3)) == Relation.Before)
 
-    forAll { (x: Interval[Int], y: Interval[Int]) =>
+    forAll { (x: (Int, Int), y: (Int, Int)) =>
       val cond = x.greater < y.lesser
       cond == ((x relate y) == Relation.Before)
     }
   }
   property("x.lesser > y.greater → After") {
-    assert((Interval(2, 2) relate Interval(0, 1)) == Relation.After)
-    assert((Interval(2, 2) relate Interval(1, 1)) == Relation.After)
-    assert((Interval(2, 3) relate Interval(1, 1)) == Relation.After)
-    assert((Interval(2, 3) relate Interval(0, 1)) == Relation.After)
+    assert(((2, 2) relate (0, 1)) == Relation.After)
+    assert(((2, 2) relate (1, 1)) == Relation.After)
+    assert(((2, 3) relate (1, 1)) == Relation.After)
+    assert(((2, 3) relate (0, 1)) == Relation.After)
 
-    forAll { (x: Interval[Int], y: Interval[Int]) =>
+    forAll { (x: (Int, Int), y: (Int, Int)) =>
       val cond = x.lesser > y.greater
       cond == ((x relate y) == Relation.After)
     }
   }
   property("x.nonEmpty ∧ x.lesser > y.lesser ∧ x.greater = y.greater → Finishes") {
-    assert((Interval(6, 7) relate Interval(3, 7)) == Relation.Finishes)
+    assert(((6, 7) relate (3, 7)) == Relation.Finishes)
 
-    forAll { (x: Interval[Int], y: Interval[Int]) =>
+    forAll { (x: (Int, Int), y: (Int, Int)) =>
       val cond = x.nonEmpty && x.lesser > y.lesser && x.greater == y.greater
       cond == ((x relate y) == Relation.Finishes)
     }
   }
   property("y.nonEmpty ∧ x.lesser < y.lesser ∧ x.greater = y.greater → FinishedBy") {
-    assert((Interval(2, 7) relate Interval(3, 7)) == Relation.FinishedBy)
+    assert(((2, 7) relate (3, 7)) == Relation.FinishedBy)
 
-    forAll { (x: Interval[Int], y: Interval[Int]) =>
+    forAll { (x: (Int, Int), y: (Int, Int)) =>
       val cond = y.nonEmpty && x.lesser < y.lesser && x.greater == y.greater
       cond == ((x relate y) == Relation.FinishedBy)
     }
   }
   property("x.lesser < y.lesser ∧ x.greater > y.greater → Contains") {
-    assert((Interval(2, 8) relate Interval(3, 7)) == Relation.Contains)
+    assert(((2, 8) relate (3, 7)) == Relation.Contains)
 
-    forAll { (x: Interval[Int], y: Interval[Int]) =>
+    forAll { (x: (Int, Int), y: (Int, Int)) =>
       val cond = x.lesser < y.lesser && x.greater > y.greater
       cond == ((x relate y) == Relation.Contains)
     }
   }
   property("x.lesser > y.lesser ∧ x.greater < y.greater → During") {
-    assert((Interval(3, 7) relate Interval(2, 8)) == Relation.During)
+    assert(((3, 7) relate (2, 8)) == Relation.During)
 
-    forAll { (x: Interval[Int], y: Interval[Int]) =>
+    forAll { (x: (Int, Int), y: (Int, Int)) =>
       val cond = x.lesser > y.lesser && x.greater < y.greater
       cond == ((x relate y) == Relation.During)
     }
   }
   property("other x < y patterns  → Overlaps") {
-    assert((Interval(2, 4) relate Interval(3, 7)) == Relation.Overlaps)
-    assert((Interval(3, 3) relate Interval(3, 7)) == Relation.Overlaps)
-    assert((Interval(3, 7) relate Interval(7, 7)) == Relation.Overlaps)
+    assert(((2, 4) relate (3, 7)) == Relation.Overlaps)
+    assert(((3, 3) relate (3, 7)) == Relation.Overlaps)
+    assert(((3, 7) relate (7, 7)) == Relation.Overlaps)
 
-    forAll { (x: Interval[Int], y: Interval[Int]) =>
+    forAll { (x: (Int, Int), y: (Int, Int)) =>
       val cond = x.lesser < y.lesser && x.greater > y.lesser && x.greater < y.greater
-               || x.isEmpty && y.nonEmpty && x.lesser == y.lesser && x.greater == y.lesser
-               || x.nonEmpty && y.isEmpty && x.greater == y.lesser && x.greater == y.greater
+              || x.isEmpty != y.isEmpty && x.lesser < y.greater && x.greater == y.lesser
       cond == ((x relate y) == Relation.Overlaps)
     }
   }
   property("other x > y patterns  → OverlappedBy") {
-    assert((Interval(3, 7) relate Interval(2, 4)) == Relation.OverlappedBy)
-    assert((Interval(3, 7) relate Interval(3, 3)) == Relation.OverlappedBy)
-    assert((Interval(7, 7) relate Interval(3, 7)) == Relation.OverlappedBy)
+    assert(((3, 7) relate (2, 4)) == Relation.OverlappedBy)
+    assert(((3, 7) relate (3, 3)) == Relation.OverlappedBy)
+    assert(((7, 7) relate (3, 7)) == Relation.OverlappedBy)
 
-    forAll { (x: Interval[Int], y: Interval[Int]) =>
+    forAll { (x: (Int, Int), y: (Int, Int)) =>
       val cond = x.lesser > y.lesser && x.lesser < y.greater && x.greater > y.greater
-               || x.isEmpty && y.nonEmpty && x.lesser == y.greater && x.greater == y.greater
-               || x.nonEmpty && y.isEmpty && x.lesser == y.lesser && x.greater > y.greater
+              || x.isEmpty != y.isEmpty && x.greater > y.lesser &&  x.lesser == y.greater
       cond == ((x relate y) == Relation.OverlappedBy)
     }
   }
   property("(x relate y).invert = y relate x") {
-    forAll { (x: Interval[Int], y: Interval[Int]) =>
+    forAll { (x: (Int, Int), y: (Int, Int)) =>
       (x relate y).invert == (y relate x)
     }
   }
